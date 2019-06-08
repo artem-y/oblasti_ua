21f9dfa4ab7ff8dddc5b385cabd20307f9a72090
Artem Yelizarov <artem.y.inbox@gmail.com>
2019-06-08 15:15:48 +0300

    Moved info viewcontroller's textfield localization to Localizable.strings

--- TestMap/Controllers/TMInfoViewController.swift
+++ TestMap/Controllers/TMInfoViewController.swift
@@ -10,15 +10,13 @@ import UIKit
 
 final class TMInfoViewController: UIViewController, TMPresentationStyleAdjustable {
     
-    // TODO: Add text
     @IBOutlet weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // TODO: Maybe, replace with other (real) text
         // Because of old Apple bug, textfields sometimes do not localize from storyboard
-        textView.text = NSLocalizedString("3IK-kW-DEU.text", tableName: "Main", bundle: Bundle(path: Bundle.path(forResource: Locale.current.languageCode!, ofType: "lproj", inDirectory: Bundle.main.bundlePath)!)!, value: "", comment: "")
+        textView.text = "Please, notice: region boundaries and borders of Ukraine are depicted approximately and can be different from real proportions and geografic coordinates.".localized()
     }
     
     // MARK: - Initialization
