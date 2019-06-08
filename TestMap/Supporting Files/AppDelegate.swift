882749d55fb0b1fc754556fda61b3e1ff7243e4c
Artem Yelizarov <artem.y.inbox@gmail.com>
2019-06-08 13:19:24 +0300

    Replaced app icon and logo

--- TestMap/Supporting Files/AppDelegate.swift
+++ TestMap/Supporting Files/AppDelegate.swift
@@ -44,13 +44,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print("application will enter foreground")
-        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        print("application did become active")
         settingsObserver?.addToNotificationCenter()
         menuModeObserver?.addToNotificationCenter()
         pauseScreenShowTimeObserver?.addToNotificationCenter()
